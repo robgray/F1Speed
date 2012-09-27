21ea88a39454b4e658f3b8fdffd7720c46218c5a
Rob Gray <rob.ragged@gmail.com>
2012-09-28 08:15:28 +1000

    Added WPF UI project

---
+++ F1Speed.Wpf/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using Caliburn.Micro;
+
+namespace F1Speed.Wpf
+{
+    public class DashboardViewModel : PropertyChangedBase
+    {
+        private string _name;
+
+        public string Name 
+        {
+            get { return _name; }
+            set 
+            {
+                if (_name == value)
+                    return;
+
+                _name = value;
+                NotifyOfPropertyChange(() => Name);
+                NotifyOfPropertyChange(() => CanSayHello);
+            }
+        }
+
+        public bool CanSayHello
+        {
+            get { return !string.IsNullOrWhiteSpace(Name);  }
+        }
+
+        public void SayHello()
+        {
+            MessageBox.Show(string.Format("Hello {0}!", Name));
+        }
+    }
+}
