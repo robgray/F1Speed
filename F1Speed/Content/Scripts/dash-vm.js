9f6e7d6c73806d0a3e876895a488f6c9d0cc7352
Robert Gray <rob.ragged@gmail.com>
2013-10-30 16:22:25 +1000

    Added new functionality for F1 2012

--- F1Speed/Content/Scripts/dash-vm.js
+++ F1Speed/Content/Scripts/dash-vm.js
@@ -11,13 +11,15 @@
     return result.toFixed(places);
 }
 
-var DashViewModel = function ($, ko) {
+var DashViewModel = function() {
 
     var self = this;
+
+    self.isSpeedPositive = ko.observable(false);
+    self.isTimePositive = ko.observable(false);
+    self.lostConnection = ko.observable(true);
     self.speedDelta = ko.observable();
     self.timeDelta = ko.observable();
-    self.speedClass = ko.observable();
-    self.timeClass = ko.observable();
     self.circuitName = ko.observable();
     self.lapType = ko.observable();
     self.wheelspinRearLeft = ko.observable(0);
@@ -26,51 +28,89 @@ var DashViewModel = function ($, ko) {
     self.wheelspinFrontRight = ko.observable(0);
     self.throttle = ko.observable(0);
     self.brake = ko.observable(0);
-    
+
+    self.speedClass = ko.computed(function() {
+        if (self.speedDelta() === 0)
+            return "";
+
+        return self.isSpeedPositive() ? "positive" : "negative";
+    });
+
+    self.timeClass = ko.computed(function() {
+        if (self.timeDelta() === 0)
+            return "";
+
+        return self.isTimePositive() ? "positive" : "negative";
+    });
+
+    self.speedDisplay = ko.computed(function() {
+        if (self.lostConnection())
+            return "ERR";
+
+        if (self.speedDelta() === 0)
+            return "0";
+
+        return (self.isSpeedPositive() ? "+" : "-") + self.speedDelta();
+    });
+
+    self.timeDisplay = ko.computed(function() {
+        if (self.lostConnection())
+            return "ERR";
+
+        if (self.timeDelta() === 0)
+            return "0";
+
+        return (self.isTimePositive() ? "-" : "+") + self.timeDelta();
+    });
+
+    self.connectionStatus = ko.computed(function() {
+        return self.lostConnection() ? "Disconnected" : "Connected";
+    });
+
+    self.timebarCss = ko.computed(function() {
+        return "timebar " + self.timeClass();
+    });
+
     function init() {
         getPacket();
     }
-    
+
     function getPacket() {
         $.ajax({
             url: "/api/packet",
             dataType: "json",
-            error: function() {
-                self.speedDelta("ERR");
-                self.timeDelta("ERR");
+            error: function () {
+                self.lostConnection(true);
+                self.speedDelta(0);
+                self.timeDelta(0);
                 self.speedClass("");
                 self.timeClass("");
-                
+
+                // Retry at a less frequent interval
                 setTimeout(function () { getPacket(); }, 2000);
             },
             success: function (data) {
-                console.log(data);
-
+                self.lostConnection(false);
+                self.isSpeedPositive(data.IsSpeedDeltaPositive);
+                self.isTimePositive(data.IsTimeDeltaPositive);
                 self.circuitName(data.CircuitName);
                 self.lapType(data.LapType);
                 self.speedDelta(round(data.SpeedDelta, 1));
                 self.timeDelta(round(data.TimeDelta, 2));
+
                 self.wheelspinRearLeft(data.WheelspinRearLeft);
                 self.wheelspinRearRight(data.WheelspinRearRight);
                 self.wheelspinFrontLeft(data.WheelspinFrontLeft);
                 self.wheelspinFrontRight(data.WheelspinFrontRight);
                 self.throttle(data.Throttle);
                 self.brake(data.Brake);
-                                
-                self.speedClass(data.IsSpeedDeltaPositive ? "positive" : "negative");
-                self.timeClass(data.IsTimeDeltaPositive ? "positive" : "negative");
-                
-                setTimeout(function() { getPacket(); }, 100);
+
+
+                setTimeout(function () { getPacket(); }, 100);
             },
             contentType: "application/json"
         });
     }
-
+    
     init();
-
-    return {
-        speedDelta: self.speedDelta,
-        timeDelta: self.timeDelta
-    };
-
-}(jQuery, ko)
+};
--- F1Speed/Content/Scripts/knockout.bindings.js
+++ F1Speed/Content/Scripts/knockout.bindings.js
@@ -17,15 +17,38 @@ ko.bindingHandlers.wheelspin = {
 };
 
 ko.bindingHandlers.pedal = {
-    init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+    init: function(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
         $(element).height(0);
     },
-    update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+    update: function(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
         var $wheelspin = $(element);
         var spinValue = ko.utils.unwrapObservable(valueAccessor());
 
-        var maxHeight = 75;
+        var maxHeight = 45;
 
         $wheelspin.height(Math.floor(maxHeight * spinValue));
     }
-}
+};
+
+ko.bindingHandlers.timeDelta = {
+    init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {        
+    },
+    update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+        var timeDelta = ko.utils.unwrapObservable(valueAccessor());
+        var isPositive = viewModel.isTimePositive();
+
+        var $bar = $(element);
+        var parentWidth = $bar.parent().width();
+        $bar.position({ top: 0, left: parentWidth / 2 });
+        
+        var maxTime = 6;
+        var barWidth = ((timeDelta / maxTime) / 2) * parentWidth;
+        $bar.width(barWidth);
+        
+        if (isPositive) {
+            $bar.css("margin-left", "" + parentWidth / 2 + "px");            
+        } else {            
+            $bar.css("margin-left", "" + (parentWidth / 2) - barWidth + "px");
+        }
+    }
+};
